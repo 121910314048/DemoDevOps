9e82eaa2ba27a704bf971d9bd9ecb5f1bb574f14
121910314048 <69963959+121910314048@users.noreply.github.com>
2021-09-22 21:27:52 +0530

    Update DemoProg.py

--- DemoProg.py
+++ DemoProg.py
@@ -1,6 +1,1 @@
-num = input("enter number :121910314048 ")
-print(num)
-name = input("enter name :sanjana ")
-print(name)
-print("type of number",type(num))
-print("type of name",type(name))
+print("hello sanju")
