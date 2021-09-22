682996aa77722786ae8a75191386a6875a231281
121910314048 <69963959+121910314048@users.noreply.github.com>
2021-09-22 21:15:06 +0530

    Update DemoProg.py

--- DemoProg.py
+++ DemoProg.py
@@ -1,6 +1,6 @@
-num = input("enter number : ")
+num = input("enter number :121910314048 ")
 print(num)
-name = input("enter name : ")
+name = input("enter name :sanjana ")
 print(name)
 print("type of number",type(num))
 print("type of name",type(name))
